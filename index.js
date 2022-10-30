b1e3c17a35da0f35417c4460ad58a07c063e5e42
Juan Jose Rossalees <jjrosalesuci@gmail.com>
2022-10-30 16:03:45 -0300

    Fixes

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
 const qrcode = require('qrcode-terminal');
-const {Client, LocalAuth, NoAuth} = require('whatsapp-web.js');
+var qr = require('qr-image');
+
+const {Client, LocalAuth, NoAuth, MessageMedia} = require('whatsapp-web.js');
 const messagesRepository = require("./services/messagesRepository");
 
-const config = {
-    timeRange : '1'
-};
-
 const client = new Client({
-    authStrategy: new LocalAuth()
-    // authStrategy: new NoAuth()
+    authStrategy: new NoAuth(),
 });
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+client.on('qr', qrsata => {
+    // qrcode.generate(qr, {small: true});s
+    var qr_svg = qr.image(qrsata, { type: 'svg' });
+    qr_svg.pipe(require('fs').createWriteStream('qr.svg'));
+    console.log('new qr');
 });
 
+let interval = null;
 client.on('ready', () => {
-    console.log('Client is ready!');
-    setInterval(() => {
-        const today = new Date();
-        const second =  `${today.getSeconds()}`;
-        if (second[0] != config.timeRange) {
-            return
-        }
-        console.log('Ejecution start: ', second);
+     console.log('Client is ready!');
+     interval =  setInterval(() => {
+        console.log('Ejecution start');
+
         messagesRepository.getMessage().then((msg) => {
             console.log(msg);
             if (!msg.id) return;
             const number = msg.number;
             const text = msg.message;
+
             const chatId = number.substring(1) + "@c.us";
-            client.sendMessage(chatId, text).then(r => {
+            const media = MessageMedia.fromFilePath(`${__dirname}/promo.jpeg`);
+
+            client.sendMessage(chatId, media, {caption: text}).then(r => {
                 let newMessage = {
                     ...msg,
                     status: "DELIVERED"
@@ -45,8 +49,40 @@ client.on('ready', () => {
                 messagesRepository.updateMessage(newMessage).then(r => {});
             });
         });
-    }, 2000);
+    }, 2000); 
 });
 
 
-client.initialize();
+client.on('disconnected', ()=> {
+    clearInterval(interval);
+    interval = 0; 
+    console.log('Client Disconecte');
+});
+
+
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/qr', (req, res) => {
+    res.sendFile(path.join(__dirname+'/qr.svg'));
+});
+
+app.get('/init', (req, res) => {
+    client.initialize();
+    res.send('start')
+})
+
+app.get('/stop', (req, res) => {
+    fs.unlinkSync(path.join(__dirname+'/qr.svg'));
+    client.destroy();
+    res.send('stop')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
