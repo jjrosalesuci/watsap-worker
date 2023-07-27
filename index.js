61ed3d0d40fe16b338ead849777ce7da6666a5f1
Juan Jose Rosales <jjrosalesuci@gmail.com>
2023-07-26 23:13:36 -0300

    fix

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ client.on('ready', () => {
             const chatId = number.substring(1) + "@c.us";
             const media = MessageMedia.fromFilePath(`${__dirname}/promo14.jpeg`);
 
-            
+
             client.sendMessage(chatId, media, {caption: text}).then(r => {
                 let newMessage = {
                     ...msg,
@@ -50,6 +50,7 @@ client.on('ready', () => {
                     status: "ERROR"
                 }
                 messagesRepository.updateMessage(newMessage).then(r => {}); */
+                
             });
         });
     }, 500); 
